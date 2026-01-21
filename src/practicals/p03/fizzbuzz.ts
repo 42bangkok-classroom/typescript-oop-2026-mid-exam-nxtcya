cfb1996102a52ac4d88b497f04d46ecce5ccb8fd
unknown <quinn3nxt@gmail.com>
2026-01-21 16:17:36 +0700

    hello.ts

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,12 +1,16 @@
 // Write your code below
-let score = parseInt(process.argv[2]);
+const score = Number(process.argv[2]);
 
-if(!score || score <= 0){
-    process.exit(1);
-}else if(score % 3 === 0 && score % 5 === 0){
-    console.log("FizzBuzz");
-}else if(score % 3 === 0){
-    console.log("Fizz");
-}else if(score % 5 === 0){
-    console.log("Buzz")
+if (!isNaN(score) && score >0) {
+for (let i =1; i <= score; i++) {
+if (i %3 ===0 && i %5 ===0) {
+console.log("FizzBuzz");
+    }else if (i %3 ===0) {
+console.log("Fizz");
+    }else if (i %5 ===0) {
+console.log("Buzz");
+    }else {
+console.log(i);
+    }
+  }
 }
