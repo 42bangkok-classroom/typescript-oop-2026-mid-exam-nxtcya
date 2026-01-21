090c2e5f84724bb1daf3db75df4f94a717e9b61f
unknown <quinn3nxt@gmail.com>
2026-01-21 15:40:37 +0700

    hello.ts

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -5,13 +5,13 @@ if(isNaN(GD) || GD > 100 || GD < 0 ){
     console.log("Invalid input");
     
 }else if(GD >= 80){
-    console.log("Grad is A")
+    console.log("Grade is A")
 }else if(GD >= 70){
-    console.log("Grad is B");
+    console.log("Grade is B");
 }else if(GD >= 60){
-    console.log("Grad is C");
+    console.log("Grade is C");
 }else if(GD >= 50){
-    console.log("Grad is B");
+    console.log("Grade is B");
 }else{
-    console.log("Grad is F");
+    console.log("Grade is F");
 }
