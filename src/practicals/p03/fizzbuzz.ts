59e5f278dc785d3b8896c58de5e11b53647c5c02
unknown <quinn3nxt@gmail.com>
2026-01-21 15:54:49 +0700

    hello.ts

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -2,7 +2,7 @@
 let score = parseInt(process.argv[2]);
 
 if(!score || score <= 0){
-    process.exit(1);
+    null;
 }else if(score % 3 === 0 && score % 5 === 0){
     console.log("FizzBuzz");
 }else if(score % 3 === 0){
